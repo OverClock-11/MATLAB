08a32aaca134c040e0704d2e18e0404b9c29f7a4
OverClock-11 <zjh2020@hnu.edu.cn>
2023-03-30 12:28:16 +0800

    Matlab

---
+++ Computational_Physics/Class_6_PDE.m
@@ -0,0 +1,29 @@
+clear, clc
+%FDM解偏微分方程u(t,x)
+
+dt = 0.0001;
+dx = 0.02;
+
+t = 0:dt:0.1;
+x = 0:dx:1;
+K = size(t,2);
+I = size(x,2);
+
+u = zeros(I,K);
+u(I,:) = 100;
+
+eps = 1e-5;
+err = 10;
+r = 0.835*dt/dx^2;
+while (err > eps)
+    u1 = u;
+    for i = 2:I - 1
+        for k = 1:K - 1
+            u(i,k + 1) = u(i,k) + r*(u(i + 1,k) - 2*u(i,k) + u(i - 1,k));
+        end
+    end
+    err = norm(u - u1,1);
+end
+
+heatmap(u,'GridVisible','off');
+
---
+++ Computational_Physics/Class_6_PDE2.m
@@ -0,0 +1,33 @@
+clear, clc
+%FDM解偏微分方程u(x,y)
+
+dx = 0.05;
+dy = 0.05;
+
+x = 0:dx:pi;
+y = 0:dy:pi;
+m = size(x,2);
+n = size(y,2);
+
+u = zeros(m,n);
+u(m,:) = sin(pi*y);
+u(:,n) = sin(pi*x)';
+
+eps = 1e-5;
+err = 10;
+while (err > eps)
+    u1 = u;
+    for i = 2:m - 1
+        for j = 2:n - 1
+            u(i,j) = (dx^2*(u(i+1,j) + u(i-1,j)) + dy^2*(u(i,j+1) + u(i,j-1)) + (dx*dy)^2*(x(i)^2 + y(j)^2)*sin(x(i)*y(j)))/(2*(dx^2 + dy^2));
+        end
+    end
+    err = norm(u - u1,1);
+end
+
+g1 = nexttile;
+a = heatmap(u,'GridVisible','off');
+
+g2 = nexttile;
+mesh(x,y,u)
+view([65.12 35.29])
